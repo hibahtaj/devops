7c94be67de1169e3a7c96b51601497f800bbc009
hibahtaj <hibahtaj2610@gmail.com>
2026-07-09 15:44:09 +0530

    Added html pages and updated app.py

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World!"
+    return render_template('reg.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    roll = request.form.get('roll')
+    year = request.form.get('year')
+
+    return render_template('success.html', name=name, year=year)
 
 if __name__ == '__main__':
     app.run(debug=True)
