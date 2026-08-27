e19958541d1d13584fb7c3b7ef2e0dff4e048841
Hibah Taj <hibahtaj@Hafsas-MacBook-Air.local>
2026-08-27 15:17:47 +0530

    jenkins file

--- app.py
+++ app.py
@@ -16,4 +16,4 @@ def submit():
     return render_template('success.html', name=name, year=year)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port = 5000, debug=True)
+    app.run(host="0.0.0.0", port = 5000, debug=True)
